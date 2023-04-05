ddd1f41e37319af3391b44c35a0a1484bbf61e88
sangminlee98 <tkdals0975@naver.com>
2023-04-06 08:41:52 +0900

    refact : TodoList에서 추가 컴포넌트 Page로 분리

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,6 +1,9 @@
 import TodoList from "@/components/TodoList";
+import useInput from "@/hooks/useInput";
 import { getTodosAPI } from "@/services/todo";
 import { Todo } from "@/types/todo";
+import onKeydown from "@/utils/onKeydown";
+import TodoPresenter from "@/utils/todoPresenter";
 import React from "react";
 import { useEffect, useState } from "react";
 import { Navigate, useOutletContext } from "react-router-dom";
@@ -8,6 +11,18 @@ import { Navigate, useOutletContext } from "react-router-dom";
 export function TodoPage() {
   const authState = useOutletContext();
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, handleTitle, setTitle] = useInput("");
+  const presenter = new TodoPresenter(todos);
+
+  // keydown 이벤트가 일어날 때 실행할 핸들러
+  const keydownHandler = () => {
+    handleTodoCreate();
+  };
+
+  const handleTodoCreate = () => {
+    presenter.create(title, setTodos);
+    setTitle("");
+  };
   useEffect(() => {
     if (!authState) return;
     getTodosAPI().then((todos) => {
@@ -16,10 +31,27 @@ export function TodoPage() {
       }
     });
   }, [authState]);
+
   if (!authState) {
     return <Navigate to="/signin" />;
   }
-  return <TodoList todos={todos} setTodos={setTodos} />;
+  return (
+    <div>
+      <div>
+        <input
+          data-testid="new-todo-input"
+          type="text"
+          value={title}
+          onChange={handleTitle}
+          onKeyDown={(e) => onKeydown(e, keydownHandler)}
+        />
+        <button data-testid="new-todo-add-button" onClick={handleTodoCreate}>
+          추가
+        </button>
+      </div>
+      <TodoList todos={todos} presenter={presenter} setTodos={setTodos} />
+    </div>
+  );
 }
 
 export default React.memo(TodoPage);
