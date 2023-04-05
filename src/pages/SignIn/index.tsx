69decfa7401fabbdf0da36a2356ded6c7d0c96a9
sangminlee98 <tkdals0975@naver.com>
2023-04-05 20:30:23 +0900

    feat : [로그인] 유효성 검사, API 연동 및 로직 구현

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,3 +1,69 @@
+import useCheckInput from "@/hooks/useCheckInput";
+import useInput from "@/hooks/useInput";
+import { SignInAPI } from "@/services/user";
+import { Form } from "@/types/form";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function SignInPage() {
-  return <div>signin</div>;
+  const navigate = useNavigate();
+  const [formDisabled, setFormDisabled] = useState(true);
+  const [email, emailHandler] = useInput("");
+  const [password, passwordHandler] = useInput("");
+  const emailErrorState = useCheckInput(email, /.*@.*/g);
+  const passwordErrorState = useCheckInput(password, /^.{8,}$/g);
+  const handleSubmit = async () => {
+    const form: Form = {
+      email,
+      password,
+    };
+    const response = await SignInAPI(form);
+    if (response) {
+      navigate("/todo");
+    }
+  };
+
+  useEffect(() => {
+    if (!emailErrorState && !passwordErrorState) {
+      setFormDisabled(false);
+    } else {
+      setFormDisabled(true);
+    }
+  }, [emailErrorState, passwordErrorState]);
+  return (
+    <div>
+      <div>
+        <label htmlFor="email">이메일</label>
+        <input
+          id="email"
+          data-testid="email-input"
+          type="text"
+          placeholder="이메일을 입력하세요."
+          value={email}
+          onChange={emailHandler}
+        />
+        <p>{emailErrorState && "@를 포함해야 합니다."}</p>
+      </div>
+      <div>
+        <label htmlFor="password">패스워드</label>
+        <input
+          id="password"
+          data-testid="password-input"
+          type="password"
+          placeholder="비밀번호를 입력하세요."
+          value={password}
+          onChange={passwordHandler}
+        />
+        <p>{passwordErrorState && "8자 이상이어야 합니다."}</p>
+      </div>
+      <button
+        type="button"
+        data-testid="signin-button"
+        onClick={handleSubmit}
+        disabled={formDisabled}
+      >
+        로그인
+      </button>
+    </div>
+  );
 }
--- src/services/user.ts
+++ src/services/user.ts
@@ -13,3 +13,25 @@ export const SignUpAPI = async (form: Form) => {
     alert((e as AxiosError<any>).response?.data.message);
   }
 };
+
+export const SignInAPI = async (form: Form) => {
+  try {
+    const response = await axios.post("/auth/signin", form, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    localStorage.setItem("token", response.data.access_token);
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${response.data.access_token}`;
+    return response;
+  } catch (e) {
+    const err = e as AxiosError<any>;
+    if (err.response?.status === 401) {
+      alert("비밀번호가 일치하지 않습니다.");
+    } else {
+      alert(err.response?.data.message);
+    }
+  }
+};
