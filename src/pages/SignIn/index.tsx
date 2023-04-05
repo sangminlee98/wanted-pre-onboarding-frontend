0884674c48effcd2f5db68df0892f86796a3f1a2
sangminlee98 <tkdals0975@naver.com>
2023-04-05 21:51:43 +0900

    style : [로그인] 스타일 적용

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
 import useCheckInput from "@/hooks/useCheckInput";
 import useInput from "@/hooks/useInput";
+import onKeydown from "@/utils/onKeydown";
 import { SignInAPI } from "@/services/user";
 import { Form } from "@/types/form";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./styles.module.scss";
 
 export default function SignInPage() {
   const navigate = useNavigate();
@@ -12,6 +14,7 @@ export default function SignInPage() {
   const [password, passwordHandler] = useInput("");
   const emailErrorState = useCheckInput(email, /.*@.*/g);
   const passwordErrorState = useCheckInput(password, /^.{8,}$/g);
+
   const handleSubmit = async () => {
     const form: Form = {
       email,
@@ -23,6 +26,12 @@ export default function SignInPage() {
     }
   };
 
+  // keydown 이벤트가 일어날 때 실행할 핸들러
+  const keydownHandler = () => {
+    if (formDisabled) return;
+    handleSubmit();
+  };
+
   useEffect(() => {
     if (!emailErrorState && !passwordErrorState) {
       setFormDisabled(false);
@@ -31,39 +40,48 @@ export default function SignInPage() {
     }
   }, [emailErrorState, passwordErrorState]);
   return (
-    <div>
-      <div>
-        <label htmlFor="email">이메일</label>
-        <input
-          id="email"
-          data-testid="email-input"
-          type="text"
-          placeholder="이메일을 입력하세요."
-          value={email}
-          onChange={emailHandler}
-        />
-        <p>{emailErrorState && "@를 포함해야 합니다."}</p>
-      </div>
-      <div>
-        <label htmlFor="password">패스워드</label>
-        <input
-          id="password"
-          data-testid="password-input"
-          type="password"
-          placeholder="비밀번호를 입력하세요."
-          value={password}
-          onChange={passwordHandler}
-        />
-        <p>{passwordErrorState && "8자 이상이어야 합니다."}</p>
-      </div>
-      <button
-        type="button"
-        data-testid="signin-button"
-        onClick={handleSubmit}
-        disabled={formDisabled}
-      >
-        로그인
-      </button>
+    <div className={styles.pageWrapper}>
+      <h1 className={styles.title}>로그인</h1>
+      <form className={styles.formWrapper}>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="email">이메일</label>
+          <input
+            id="email"
+            data-testid="email-input"
+            type="text"
+            placeholder="이메일을 입력하세요."
+            value={email}
+            onChange={emailHandler}
+          />
+          <p className={styles.errorMessage}>
+            {emailErrorState && "@를 포함해야 합니다."}
+          </p>
+        </div>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="password">패스워드</label>
+          <input
+            id="password"
+            data-testid="password-input"
+            type="password"
+            placeholder="비밀번호를 입력하세요."
+            value={password}
+            onChange={passwordHandler}
+            onKeyDown={(e) => onKeydown(e, keydownHandler)}
+          />
+          <p className={styles.errorMessage}>
+            {passwordErrorState && "8자 이상이어야 합니다."}
+          </p>
+        </div>
+        <button
+          className={styles.button}
+          type="button"
+          data-testid="signin-button"
+          onClick={handleSubmit}
+          disabled={formDisabled}
+        >
+          로그인
+        </button>
+      </form>
     </div>
   );
 }
