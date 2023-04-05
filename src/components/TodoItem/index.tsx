45b4d68999b792f6a3c39e2349c2b1b0999da862
sangminlee98 <tkdals0975@naver.com>
2023-04-06 06:55:16 +0900

    feat : updateTodoAPI 중 todo input 업데이트 API 연동 및 구현

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,5 +1,7 @@
+import useInput from "@/hooks/useInput";
 import { Todo } from "@/types/todo";
 import TodoPresenter from "@/utils/todoPresenter";
+import { useState } from "react";
 
 type TodoItemProps = {
   todo: Todo;
@@ -8,25 +10,64 @@ type TodoItemProps = {
 };
 
 export default function TodoItem({ todo, presenter, setTodos }: TodoItemProps) {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [updateInput, handleUpdateInput] = useInput(todo.todo);
   const handleTodoDelete = () => {
     presenter.delete(todo, setTodos);
   };
-  const handleTodoCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTodoCheckUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
     const updatedTodo = { ...todo, isCompleted: e.target.checked };
     presenter.update(updatedTodo, setTodos);
   };
+  const handleTodoInputUpdate = () => {
+    const updatedTodo = { ...todo, todo: updateInput };
+    presenter.update(updatedTodo, setTodos);
+    setIsUpdating(false);
+  };
+
+  function UpdatingInput() {
+    return (
+      <>
+        <input
+          data-testid="modify-input"
+          value={updateInput}
+          onChange={handleUpdateInput}
+        />
+        <button data-testid="submit-button" onClick={handleTodoInputUpdate}>
+          제출
+        </button>
+        <button
+          data-testid="cancel-button"
+          onClick={() => setIsUpdating(false)}
+        >
+          취소
+        </button>
+      </>
+    );
+  }
+
+  function NoUpdatingInput() {
+    return (
+      <>
+        <p>{todo.todo}</p>
+        <button data-testid="modify-button" onClick={() => setIsUpdating(true)}>
+          수정
+        </button>
+        <button data-testid="delete-button" onClick={handleTodoDelete}>
+          삭제
+        </button>
+      </>
+    );
+  }
+
   return (
     <li>
       <input
         type="checkbox"
         checked={todo.isCompleted}
-        onChange={handleTodoCheck}
+        onChange={handleTodoCheckUpdate}
       />
-      <p>{todo.todo}</p>
-      <button data-testid="modify-button">수정</button>
-      <button data-testid="delete-button" onClick={handleTodoDelete}>
-        삭제
-      </button>
+      {isUpdating ? <UpdatingInput /> : <NoUpdatingInput />}
     </li>
   );
 }
